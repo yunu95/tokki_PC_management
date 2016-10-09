59d692abae52fc80720b5128c577db8d73546e3e
albtraum <properhuman@gmail.com>
2016-10-10 01:25:16 +0900

    오로록

--- PC.cpp
+++ PC.cpp
@@ -11,7 +11,32 @@ PC::~PC()
 {
 }
 
+
+bool PC::StartUsing() {
+	is_active = true;
+	return true;
+}
+//����ڰ� ��ǻ�͸� ��������μ� �������� ��� ������ 
+//is_active �� true�� �ȴ�.
+
+bool PC::StopUsing() {
+	is_active = false;
+	return true;
+}
+//����ڰ� ��ǻ�͸� ����� �������μ� �������� ��� ������ 
+//is_active �� false�� �ȴ�.
+
 bool PC::TurnOnComputer() {
 	is_power_on = true;
 	return true;
+}}
+//����ڰ� ��ǻ���� ������ �����μ� ���������� ��� ������ 
+//is_power_on �� true�� �ȴ�.
+
+bool PC::TurnOffComputer() {
+	is_power_on = false;
+	return true;
 }
+//����ڰ� ��ǻ���� ������ �����μ� ���������� ��� ������ 
+//is_power_on �� false�� �ȴ�.
+//code by Vincent
