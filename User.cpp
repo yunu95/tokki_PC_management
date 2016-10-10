1ac5a1fb698a0d6c5302c3524450f6310b71a936
NOorYes <1313susu@gmail.com>
2016-10-10 20:13:02 +0900

    test

--- User.cpp
+++ User.cpp
@@ -4,6 +4,12 @@
 
 User::User()
 {
+	// 사용시간이 만료되었을 때 target_pc에게 강제종료를 요청하는 메서드입니다.
+	void AbortUsing(const PC& target_pc)
+	{}
+	// 사용자의 남은 시간을 설정하는 메서드입니다.
+	void SetLeftTime(const float& time)
+	{}
 }
 
 
