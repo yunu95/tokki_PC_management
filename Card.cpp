83cb68dc78c9b1d5d267b063fa78bd97a7fdd1b3
NOorYes <1313susu@gmail.com>
2016-10-12 17:16:15 +0900

    apple

--- Card.cpp
+++ Card.cpp
@@ -1,12 +1,12 @@
 #include "Card.h"
 
-
-
-Card::Card()
+Card::Card(const int& _card_number, float _left_time) // constructor
 {
+	card_number = _card_number; 
+	left_time = _left_time;
 }
 
 
-Card::~Card()
+Card::~Card() // destructor
 {
 }
