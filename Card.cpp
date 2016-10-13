3eaf8bac07923d03a30eca6dfe4b4d464d511dfc
NoorYes <1313susu@gmail.com>
2016-10-13 13:38:54 +0900

    apple

--- Card.cpp
+++ Card.cpp
@@ -1,6 +1,6 @@
 #include "Card.h"
 
-Card::Card(const int& _card_number, float _left_time) // constructor
+Card::Card(const int& _card_number, float& _left_time) // constructor
 {
 	card_number = _card_number; 
 	left_time = _left_time;
